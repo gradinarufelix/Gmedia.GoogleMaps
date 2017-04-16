1318cd2a9eff8dcd9416ed977cd28f81f25dd399
Felix Gradinaru <felix@g-media.at>
2017-04-16 16:00:48 +0200

    Initial Commit 16/04/2017

---
+++ Classes/Gmedia/GoogleMaps/ViewHelpers/MapViewHelper.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Gmedia\GoogleMaps\ViewHelpers;
+
+use TYPO3\Flow\Utility\Algorithms;
+
+class MapViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+	
+	/**
+     * @var boolean
+     */
+    protected $escapeOutput = false;
+	
+	/**
+	 * @var array
+	 */
+	protected $settings;
+	  
+	/**
+	 * Inject settings
+	 *
+	 * @param array $settings
+	 * @return void
+	 */
+	public function injectSettings(array $settings) {
+	    $this->settings = $settings;
+	}
+	
+    /**
+     * Render the title and apply some magic
+     *
+     * @param string $containerClass container class
+     * @param string $title title of the site header
+     * @param string $lead lead text of the site header
+     * @param string $leadClass class of the lead text
+     * @param string $buttons html of the content section
+     * @return string rendered html
+     * @throws \TYPO3\Fluid\Core\ViewHelper\Exception
+     */
+    public function render($lat = 0, $long = 0, $zoom = 8, $height = "500px") {
+	    	// Create Object ID
+			$uuid = Algorithms::generateUUID();
+			$apiKey = $this->settings["apiKeys"]["google"];
+			
+            $output = "<div class='gc-map'><div id='map-".$uuid."'></div>";
+            
+		    $output .= '<script>
+		    var map;
+		    function initMap() {
+			    map = new google.maps.Map(document.getElementById("map-'.$uuid.'"), {
+				  center: {lat: '.$lat.', lng: '.$long.'},
+				  zoom: '.$zoom.'
+				});
+				
+				var marker = new google.maps.Marker({
+		          map: map,
+		          position: {lat: '.$lat.', lng: '.$long.'}
+		        });
+			}
+		    </script>
+		    <style>#map-'.$uuid.' { height: '.$height.'; position: relative; }</style>';
+		    //$output .= '<script async defer
+		    //  src="https://maps.googleapis.com/maps/api/js?key='.$apiKey.'&callback=initMap">
+		    //</script>';
+		    
+				
+			$output .= '</div>';
+			
+			return $output;
+    }
+}	
+	
+?>
